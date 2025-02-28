5f280a88da57414428a10197492614d8953c828d
Martin Suarez <ms.prgramador@gmail.com>
2025-02-28 18:37:57 -0300

    add data to classes

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,14 @@ class Person(models.Model):
   name = models.CharField(max_length=30)
   age = models.IntegerField()
 
+  def __str__(self):
+    return self.name
+
+
 class Car(models.Model):
   name = models.CharField(max_length=30)
   color = models.CharField(max_length=30)
   person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
